db9237bb3e81a985c21ac905bbb9f68805654e49
JanAdell <36292051+JanAdell@users.noreply.github.com>
2019-04-20 19:16:20 +0200

    Added Player 2 module file and base content on thers

---
+++ Street Fighter II/Street-Fighter-II/ModulePlayer.cpp
@@ -0,0 +1,68 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleTextures.h"
+#include "ModuleInput.h"
+//#include "ModuleParticles.h"
+//#include "ModuleRender.h"
+//#include "ModuleCollision.h"
+#include "ModuleFadeToBlack.h"
+//#include "ModuleFonts.h"
+#include "ModulePlayer.h"
+
+#include<stdio.h>
+
+
+ModulePlayer::ModulePlayer()
+{
+	
+}
+
+ModulePlayer::~ModulePlayer()
+{}
+
+// Load assets
+bool ModulePlayer::Start()
+{
+	LOG("Loading player");
+
+	/*graphics = App->textures->Load("rtype/ship.png");
+
+	destroyed = false;
+	position.x = 150;
+	position.y = 120;
+	score = 0;
+
+	col = App->collision->AddCollider({ position.x, position.y, 32, 16 }, COLLIDER_PLAYER, this);
+
+
+	font_score = App->fonts->Load("fonts/rtype_font.png", "! @,_./0123456789$;<&?abcdefghijklmnopqrstuvwxyz", 1);
+
+
+
+	return true;*/
+}
+
+// Unload assets
+bool ModulePlayer::CleanUp()
+{
+	LOG("Unloading player");
+
+	/*App->textures->Unload(graphics);
+	App->fonts->UnLoad(font_score);
+	if (col)
+		col->to_delete = true;
+
+	return true;*/
+}
+
+// Update: draw background
+update_status ModulePlayer::Update()
+{
+
+}
+
+void ModulePlayer::OnCollision(Collider* c1, Collider* c2)
+{
+	
+	
+}
--- Street Fighter II/Street-Fighter-II/ModuleWindow.h
+++ Street Fighter II/Street-Fighter-II/ModuleWindow.h
@@ -1,8 +1,32 @@
 #ifndef __ModuleWindow_H__
 #define __ModuleWindow_H__
 
-const int WINDOW_WIDTH = 1040;
-const int WINDOW_HEIGHT = 680;
+#include "Module.h"
 
+struct SDL_Window;
+struct SDL_Surface;
 
-#endif 
+class ModuleWindow : public Module
+{
+public:
+
+	ModuleWindow();
+
+	// Destructor
+	virtual ~ModuleWindow();
+
+	// Called before quitting
+	bool Init();
+
+	// Called before quitting
+	bool CleanUp();
+
+public:
+	//The window we'll be rendering to
+	SDL_Window* window = nullptr;
+
+	//The surface contained by the window
+	SDL_Surface* screen_surface = nullptr;
+};
+
+#endif // __ModuleWindow_H__
