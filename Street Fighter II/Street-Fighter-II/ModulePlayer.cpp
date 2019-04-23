feecb217081638c316cf02848e5f86e6bdc227db
oscarrep <oscar.bcn.95@gmail.com>
2019-04-23 02:03:54 +0200

    ryu now moves forwards and backwards

--- Street Fighter II/Street-Fighter-II/ModulePlayer.cpp
+++ Street Fighter II/Street-Fighter-II/ModulePlayer.cpp
@@ -22,47 +22,47 @@ ModulePlayer::ModulePlayer()
 	idle.PushBack({ 424, 827, 58, 92 });
 	idle.PushBack({ 147, 919, 55, 93 });
 	//idle.PushBack({ 366, 12, 60, 92 });
-	idle.speed = 0.18f;
+	idle.speed = 0.1f;
 
 	// walk forward animation (arcade sprite sheet)
 	forward.PushBack({ 546, 474, 53, 83 });
-	forward.PushBack({ 786, 558, 60, 88 });
+	forward.PushBack({ 789, 558, 60, 88 });
 	forward.PushBack({ 844, 827, 64, 92 });
 	forward.PushBack({ 853, 646, 63, 90 });
 	forward.PushBack({ 195, 827, 54, 91 });
-	forward.PushBack({ 376, 646, 50, 89 });
+	forward.PushBack({ 379, 646, 50, 89 });
 	forward.speed = 0.1f;
 
 	// walk backwards animation
-	backward.PushBack({ 311, 649, 57, 89 });
-	backward.PushBack({ 388, 560, 61, 87 });
-	backward.PushBack({ 534, 649, 59, 90 });
-	backward.PushBack({ 0, 739, 57, 90 });
-	backward.PushBack({ 710, 646, 58, 90 });
-	backward.PushBack({ 905, 739, 58, 91 });
+	backward.PushBack({ 264, 646, 57, 89 });
+	backward.PushBack({ 467, 558, 61, 87 });
+	backward.PushBack({ 640, 646, 59, 90 });
+	backward.PushBack({ 0, 736, 57, 90 });
+	backward.PushBack({ 395, 736, 58, 90 });
+	backward.PushBack({ 0, 827, 58, 91 });
 	backward.speed = 0.1f;
 
 	// punch animation
-	punch.PushBack({ 759, 739, 64, 91 });
-	punch.PushBack({ 813, 739, 92, 91 });
+	punch.PushBack({ 131, 827, 64, 91 });
+	punch.PushBack({ 861, 736, 92, 91 });
 	punch.speed = 0.1f;
 
 	// kick animation
-	kick.PushBack({ 375, 830, 66, 92 });
-	kick.PushBack({ 617, 923, 114, 94 });
+	kick.PushBack({ 358, 827, 66, 92 });
+	kick.PushBack({ 747, 919, 114, 94 });
 	kick.speed = 0.1f;
 
 	// neutral jump animation
-	njump.PushBack({ 746, 1114, 56, 104 });
-	njump.PushBack({ 193, 649, 50, 89 });
-	njump.PushBack({ 538, 319, 54, 77 });
+	njump.PushBack({ 844, 1108, 56, 104 });
+	njump.PushBack({ 542, 646, 50, 89 });
+	njump.PushBack({ 827, 319, 54, 77 });
 	njump.PushBack({ 315, 243, 48, 70 });
-	njump.PushBack({ 486, 649, 48, 89 });
-	njump.PushBack({ 55, 1219, 55, 109 });
+	njump.PushBack({ 592, 646, 48, 89 });
+	njump.PushBack({ 252, 1212, 55, 109 });
 	njump.speed = 0.1f;
 
 	//forward jump animation
-	fjump.PushBack({ 55, 1219, 55, 109 });
+	fjump.PushBack({ 252, 1212, 55, 109 });
 	fjump.PushBack({ 814, 396, 61, 78 });
 	fjump.PushBack({ 387, 0, 104, 42 });
 	fjump.PushBack({ 130, 476, 53, 82 });
@@ -122,15 +122,20 @@ update_status ModulePlayer::Update()
 
 	int speed = 1;
 
-	if (App->input->keyboard[SDL_SCANCODE_D] == 1)
+	if (App->input->keyboard[SDL_SCANCODE_D] == KEY_REPEAT)
 	{
 		current_animation = &forward;
 		position.x += speed;
 	}
-	if (App->input->keyboard[SDL_SCANCODE_A] == 1)
+	if (App->input->keyboard[SDL_SCANCODE_A] == KEY_REPEAT)
 	{
 		current_animation = &backward;
 		position.x -= speed;
+	}
+	if (App->input->keyboard[SDL_SCANCODE_W] == 1)
+	{
+		current_animation = &njump;
+		position.y -= speed;
 	}
 
 	// Draw everything --------------------------------------
