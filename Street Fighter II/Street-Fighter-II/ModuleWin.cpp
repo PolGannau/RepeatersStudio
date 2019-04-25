4b3002e2a733b517d7e6d1af9cdc65ec13666ac6
Pol Gannau Coma <36154511+PolGannau@users.noreply.github.com>
2019-04-25 11:35:44 +0200

    ModuleWin Update
    Entered the image to oad and the blit to module win.

--- Street Fighter II/Street-Fighter-II/ModuleWin.cpp
+++ Street Fighter II/Street-Fighter-II/ModuleWin.cpp
@@ -1,20 +1,33 @@
 #include "ModuleWin.h"
+#include "Application.h"
+#include "ModuleTextures.h"
+#include "Globals.h"
+#include "ModuleWindow.h"
+#include "ModuleRender.h"
 
 ModuleWin::ModuleWin()
-{
-}
+{}
 
 ModuleWin::~ModuleWin()
-{
-}
+{}
 
 bool ModuleWin::Start()
 {
+	LOG("loading Win Scene");
+	//We create a rect to put the winner texture in it.
+	texture = App->textures->Load("Assets/Images/Ui/UiSprites-1.png");
+	winner_rect = { 265, 1157, 265, 354 };
+	
+
 	return false;
 }
 
 update_status ModuleWin::Update()
 {
+	if (update_status::UPDATE_ERROR)
+		LOG("Could not update Win Scene");
+
+	App->render->Blit(texture, 128, 56, winner_rect, 1.0F, true);
 	return update_status();
 }
 
--- Street Fighter II/Street-Fighter-II/ModuleWin.h
+++ Street Fighter II/Street-Fighter-II/ModuleWin.h
@@ -19,6 +19,9 @@ public:
 
 	SDL_Texture* background = nullptr;
 	uint fx = 0;
+	SDL_Texture* texture = nullptr;
+	SDL_Rect* winner_rect;
+	SDL_Rect* loser_rect;
 };
 
 #endif // !__MODULEWIM_H__
