0e50a48493f28c64eee5fef57651b2fd004e6e66
JanAdell <36292051+JanAdell@users.noreply.github.com>
2019-04-27 13:58:59 +0200

    Music in Loose Screen working correctly

--- Street Fighter II/Street-Fighter-II/ModuleLose.cpp
+++ Street Fighter II/Street-Fighter-II/ModuleLose.cpp
@@ -23,8 +23,8 @@ bool ModuleLose::Start()
 {
 	LOG("Startin Lose Scene.......");
 	
-	loose_music = App->audio->LoadMusic("Assets/Audio/Soundtrack/14 Continue....ogg");
-
+	loose_music = App->audio->LoadMusic("Assets/Audio/Soundtrack/15 Game Over!.ogg");
+	App->audio->PlayMusic(loose_music);
 	//disable the player and stage
 	App->player->Disable();
 	App->ryu_stage->Disable();
@@ -43,7 +43,7 @@ update_status ModuleLose::Update()
 {
 	if (update_status::UPDATE_ERROR)
 		LOG("Could not update lose scene");
-	App->audio->PlayMusic(loose_music);
+	
 	App->render->Blit(texture_loser, 50, 50, NULL);
 	App->render->Blit(texture_winner, 100, 100, NULL);
 	App->render->Blit(texture_text, 50, 150, &text_rect, NULL);
