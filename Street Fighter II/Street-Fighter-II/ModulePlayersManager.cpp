d1116f8a4a83345a8324c599d233e23a468bd1d2
Pol Gannau Coma <36154511+PolGannau@users.noreply.github.com>
2019-06-07 22:07:04 +0200

    ModulePlayerManager
    Added the sample functions to moduleplayermanager.cpp

---
+++ Street Fighter II/Street-Fighter-II/ModulePlayersManager.cpp
@@ -0,0 +1,29 @@
+#include "ModulePlayersManager.h"
+
+ModulePlayersManager::ModulePlayersManager()
+{}
+
+ModulePlayersManager::~ModulePlayersManager()
+{}
+
+bool ModulePlayersManager::Start()
+{
+
+
+	return true;
+}
+
+bool ModulePlayersManager::CleanUp()
+{
+
+
+	return true;
+}
+
+update_status ModulePlayersManager::Update()
+{
+
+
+	return update_status::UPDATE_CONTINUE;
+}
+
