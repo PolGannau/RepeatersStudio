f6894c57af9bb2a3d2f807ff5c71eb8606321507
MarcRosellH <36154632+MarcRosellH@users.noreply.github.com>
2019-06-09 18:09:17 +0200

    Module Animation modified
    Now every frame will save its own collider positions

--- Street Fighter II/Street-Fighter-II/Animation.h
+++ Street Fighter II/Street-Fighter-II/Animation.h
@@ -4,16 +4,28 @@
 #include "../SDL/include/SDL_rect.h"
 #define MAX_FRAMES 100
 
+struct CollidersPosition
+{
+	SDL_Rect head;
+	SDL_Rect body;
+	SDL_Rect legs;
+	SDL_Rect attack;
+};
+
 class Animation
 {
 public:
 	bool loop = false;
 	float speed = 1.0f;
 	SDL_Rect frames[MAX_FRAMES];
+	CollidersPosition framenumber[MAX_FRAMES];
 
 private:
 	float current_frame = 0.0f;
+	float current_colliders = 0.0f;
 	int last_frame = 0;
+	int last_collider = 0;
+	int coll_loops = 0;
 	int loops = 0;
 
 public:
@@ -31,6 +43,14 @@ public:
 		frames[last_frame++] = rect;
 	}
 
+	void CollPushBack(const SDL_Rect& head,const SDL_Rect& body, const SDL_Rect& legs, const SDL_Rect& attack)
+	{
+		framenumber[last_collider].head = head;
+		framenumber[last_collider].body = body;
+		framenumber[last_collider].legs = legs;
+		framenumber[last_collider++].attack = attack;
+	}
+
 	SDL_Rect& GetCurrentFrame()
 	{
 		current_frame += speed;
@@ -43,6 +63,17 @@ public:
 		return frames[(int)current_frame];
 	}
 
+	CollidersPosition& GetCurrentColliders()
+	{
+		current_colliders += speed;
+		if (current_colliders >= last_collider)
+		{
+			current_colliders = (loop) ? 0.0f : last_collider - 1;
+			coll_loops++;
+		}
+		return framenumber[(int)current_colliders];
+	}
+
 	bool Finished() const
 	{
 		return loops > 0;
@@ -50,7 +81,9 @@ public:
 
 	void Reset()
 	{
+		coll_loops = 0;
 		loops = 0;
+		current_colliders = 0.0f;
 		current_frame = 0.0f;
 	}
 };
