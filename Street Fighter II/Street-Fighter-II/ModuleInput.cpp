b633b01a9a4f59cec301d1e03fdbbd8791040d3d
oscarrep <oscar.bcn.95@gmail.com>
2019-06-09 14:41:22 +0200

    player 1 moves with controller

--- Street Fighter II/Street-Fighter-II/ModuleInput.cpp
+++ Street Fighter II/Street-Fighter-II/ModuleInput.cpp
@@ -27,6 +27,33 @@ bool ModuleInput::Init()
 		ret = false;
 	}
 
+	// controller
+	LOG("Init controller");
+
+	if (SDL_InitSubSystem(SDL_INIT_GAMECONTROLLER) < 0)
+	{
+		LOG("GamePad controller could not initialize! SDL_Error: %s\n", SDL_GetError());
+	}
+
+
+	int maxJoys = SDL_NumJoysticks();
+
+
+	for (int i = 0; i < maxJoys; ++i)
+
+	{
+		if (i >= MAX_GAMEPADS) break;
+
+		if (SDL_IsGameController(i))
+		{
+			gamepad[i] = SDL_GameControllerOpen(i);
+		}
+		if (SDL_IsGameController(i))
+		{
+			gamepad2[i] = SDL_GameControllerOpen(i);
+		}
+	}
+
 	return ret;
 }
 
@@ -61,6 +88,131 @@ update_status ModuleInput::PreUpdate()
 
 	if (keyboard[SDL_QUIT]) {
 		return update_status::UPDATE_STOP;
+	}
+
+	// Controller
+
+	for (int i = 0; i < MAX_GAMEPADS; ++i)
+	{
+		if (gamepad[i] != nullptr)
+		{
+			controller_state[BUTTON_A] = SDL_GameControllerGetButton(gamepad[i], SDL_CONTROLLER_BUTTON_A);
+		}
+		if (gamepad[i] != nullptr)
+		{
+			controller_state[BUTTON_B] = SDL_GameControllerGetButton(gamepad[i], SDL_CONTROLLER_BUTTON_B);
+		}
+		if (gamepad[i] != nullptr)
+		{
+			controller_state[BUTTON_X] = SDL_GameControllerGetButton(gamepad[i], SDL_CONTROLLER_BUTTON_X);
+		}
+		if (gamepad[i] != nullptr)
+		{
+			controller_state[BUTTON_Y] = SDL_GameControllerGetButton(gamepad[i], SDL_CONTROLLER_BUTTON_Y);
+		}
+		/*if (gamepad[i]!=nullptr)
+		{
+			controller_state[BUTTON_DPAD_DOWN] = SDL_GameControllerGetButton(gamepad[i], SDL_CONTROLLER_BUTTON_DPAD_DOWN);
+		}
+		if (gamepad[i] != nullptr)
+		{
+			controller_state[BUTTON_DPAD_UP] = SDL_GameControllerGetButton(gamepad[i], SDL_CONTROLLER_BUTTON_DPAD_UP);
+		}
+		if (gamepad[i] != nullptr)
+		{
+			controller_state[BUTTON_DPAD_LEFT] = SDL_GameControllerGetButton(gamepad[i], SDL_CONTROLLER_BUTTON_DPAD_LEFT);
+		}
+		if (gamepad[i] != nullptr)
+		{
+			controller_state[BUTTON_DPAD_RIGHT] = SDL_GameControllerGetButton(gamepad[i], SDL_CONTROLLER_BUTTON_DPAD_RIGHT);
+		}*/
+		/*if (gamepad[i] != nullptr)
+		{
+			controller_state[BUTTON_L1] = SDL_GameControllerGetButton(gamepad[i], SDL_CONTROLLER_AXIS_TRIGGERLEFT);
+		}*/
+		if (gamepad[i] != nullptr)
+		{
+			controller_state[BUTTON_START] = SDL_GameControllerGetButton(gamepad[i], SDL_CONTROLLER_BUTTON_START);
+		}
+		if (gamepad[i] != nullptr)
+		{
+			controller_state[BUTTON_BACK] = SDL_GameControllerGetButton(gamepad[i], SDL_CONTROLLER_BUTTON_BACK);
+		}
+		/*if (gamepad2[i] != nullptr)
+		{
+		controller_state[BUTTON_A] = SDL_GameControllerGetButton(gamepad2[i], SDL_CONTROLLER_BUTTON_A);
+		}*/
+	}
+
+	for (int i = 0; i < MAX_BUTTONS; ++i)
+	{
+		if (controller_state[i] == 1) {
+			if (controller[i] == KEY_IDLE)
+				controller[i] = KEY_DOWN;
+			else
+				controller[i] = KEY_REPEAT;
+		}
+		else
+		{
+			if (controller[i] == KEY_REPEAT || controller[i] == KEY_DOWN)
+				controller[i] = KEY_UP;
+			else
+				controller[i] = KEY_IDLE;
+		}
+	}
+
+	if (Events.type == SDL_CONTROLLERDEVICEADDED)
+	{
+		if (Events.cdevice.which > MAX_GAMEPADS - 1)
+		{
+			LOG("Maximum number of gamepads reached, you cannot connect more...");
+
+		}
+
+		else if (SDL_IsGameController(Events.cdevice.which))
+		{
+			gamepad[Events.cdevice.which] = SDL_GameControllerOpen(Events.cdevice.which);
+			LOG("Controller added: %d", Events.cdevice.which);
+		}
+
+	}
+
+
+	// Controller inputs: Axis
+	for (int i = 0; i < MAX_GAMEPADS; ++i)
+	{
+		if (gamepad[i] != nullptr)
+		{
+			GamepadDir[i].X = SDL_GameControllerGetAxis(gamepad[i], SDL_CONTROLLER_AXIS_LEFTX);
+			GamepadDir[i].Y = SDL_GameControllerGetAxis(gamepad[i], SDL_CONTROLLER_AXIS_LEFTY);
+		}
+		if (gamepad2[i] != nullptr)
+		{
+			GamepadDir[i].X = SDL_GameControllerGetAxis(gamepad2[i], SDL_CONTROLLER_AXIS_LEFTX);
+			GamepadDir[i].Y = SDL_GameControllerGetAxis(gamepad2[i], SDL_CONTROLLER_AXIS_LEFTY);
+		}
+	}
+
+
+	if (GamepadDir[0].X > GamepadDir[0].deadzone)
+	{
+		keyboard[SDL_SCANCODE_D] = KEY_REPEAT;
+	}
+
+	else if (GamepadDir[0].X < -GamepadDir[0].deadzone)
+	{
+		keyboard[SDL_SCANCODE_A] = KEY_REPEAT;
+	}
+
+
+	if (GamepadDir[0].Y < -GamepadDir[0].deadzone)
+	{
+		keyboard[SDL_SCANCODE_W] = KEY_DOWN;
+	}
+
+	else if (GamepadDir[0].Y > GamepadDir[0].deadzone)
+	{
+		keyboard[SDL_SCANCODE_S] = KEY_REPEAT;
 	}
 
 	return update_status::UPDATE_CONTINUE;
--- Street Fighter II/Street-Fighter-II/ModuleInput.h
+++ Street Fighter II/Street-Fighter-II/ModuleInput.h
@@ -4,9 +4,13 @@
 #include "Module.h"
 #include "Globals.h"
 #include "../SDL/include/SDL_scancode.h"
+#include "../SDL/include/SDL_gamecontroller.h"
 #include "../SDL/include/SDL_events.h"
 
 #define MAX_KEYS 300
+#define MAX_CONTROLLERS 2
+#define MAX_GAMEPADS 2
+#define MAX_BUTTONS 10
 
 enum KEY_STATE
 {
@@ -14,6 +18,24 @@ enum KEY_STATE
 	KEY_DOWN,
 	KEY_REPEAT,
 	KEY_UP
+};
+
+enum BUTTONS
+{
+	BUTTON_A,
+	BUTTON_B,
+	BUTTON_X,
+	BUTTON_Y,
+	BUTTON_L1,
+	BUTTON_R1,
+	BUTTON_L2,
+	BUTTON_R2,
+	BUTTON_BACK,
+	BUTTON_START,
+	BUTTON_DPAD_UP,
+	BUTTON_DPAD_DOWN,
+	BUTTON_DPAD_LEFT,
+	BUTTON_DPAD_RIGHT
 };
 
 class ModuleInput : public Module
@@ -28,8 +50,29 @@ public:
 	bool CleanUp();
 
 public:
+	
 	KEY_STATE keyboard[MAX_KEYS];
 	SDL_Event Events;
+
+	KEY_STATE controller[MAX_BUTTONS];
+	KEY_STATE controller2[MAX_BUTTONS];
+
+	bool controller_state[MAX_BUTTONS];
+	SDL_GameController* gamepad[MAX_GAMEPADS] = { nullptr };
+	SDL_GameController* gamepad2[MAX_GAMEPADS] = { nullptr };
+
+	struct ControlDir
+	{
+		bool up = false;
+		bool down = false;
+		bool left = false;
+		bool right = false;
+		Sint16 X = 0;
+		Sint16 Y = 0;
+		int deadzone = 5000;
+	};
+
+	ControlDir GamepadDir[MAX_GAMEPADS];
 };
 
 #endif // __ModuleInput_H__
