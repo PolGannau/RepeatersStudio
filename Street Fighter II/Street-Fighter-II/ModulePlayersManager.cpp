ea2fa20b0ea78fc03400ccad3be8acb25a5ceda3
MarcRosellH <36154632+MarcRosellH@users.noreply.github.com>
2019-06-09 13:37:48 +0200

    Honda can onyl jump once after touching the floor

--- Street Fighter II/Street-Fighter-II/ModulePlayersManager.cpp
+++ Street Fighter II/Street-Fighter-II/ModulePlayersManager.cpp
@@ -59,7 +59,7 @@ update_status ModulePlayersManager::Update()
 
 	/// PLAYER INPUT CONTROL AND LOGIC ---------------------------------------------------
 	//JUMP
-	if (App->input->keyboard[SDL_SCANCODE_W] == KEY_DOWN)
+	if (App->input->keyboard[SDL_SCANCODE_W] == KEY_DOWN && player->state == ON_FLOOR)
 	{
 		player->state = JUMPING;
 		player->vspeed += player->VerticalSpeed;
@@ -110,7 +110,7 @@ update_status ModulePlayersManager::Update()
 
 	/// PLAYER 2 INPUT CONTROL AND LOGIC -------------------------------------------------
 	//JUMP
-	if (App->input->keyboard[SDL_SCANCODE_UP] == KEY_DOWN)
+	if (App->input->keyboard[SDL_SCANCODE_UP] == KEY_DOWN && player->state == ON_FLOOR)
 	{
 		player2->state = JUMPING;
 		player2->vspeed += player2->VerticalSpeed;
